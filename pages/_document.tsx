5e19a9a02ae72f107444c5f27f4a9a7d5527b259
Alex <theeduazizjon@gmail.com>
2026-09-11 17:53:43 +0900

    feat: meta tags inegrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name = "title" content = "Nestar Demo" />
+        <meta name = "robots"content = "index, follow" /> 
+        <link rel="icon" type = "image/png" href="public/img/logo/favicon.svg"/>
+
+        <meta name ="keyword" content = {"nestar,nestar.uz, devex mern, mern nestjs fullstack"} />
+        <meta name ="description" content = {
+          "Buy and Sell properties with Nestar. | " +
+          "Nestar is a real estate platform that allows users to buy and sell properties online. " +
+          "It is built using the MERN stack and NestJS."
+        }
+         />
+        </Head>
       <body>
         <Main />
         <NextScript />
